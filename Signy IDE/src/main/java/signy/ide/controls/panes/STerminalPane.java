200df0e9fa7a12c789bab4f63aaf86c42943a8b0
unixxcorn <siwawat.srisuk@gmail.com>
2018-10-18 02:48:19 +0700

    Console update but it still not support for interactive command.

--- Signy IDE/src/main/java/signy/ide/controls/panes/STerminalPane.java
+++ Signy IDE/src/main/java/signy/ide/controls/panes/STerminalPane.java
@@ -3,6 +3,7 @@ package signy.ide.controls.panes;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TabPane.TabClosingPolicy;
+import signy.ide.core.module.SConsoleTab;
 
 public class STerminalPane {
 
@@ -14,7 +15,7 @@ public class STerminalPane {
 		tabPane.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
 		tabPane.getTabs().add(new Tab("Problems"));
 		tabPane.getTabs().add(new Tab("Output"));
-		tabPane.getTabs().add(new Tab("Console"));
+		tabPane.getTabs().add(new SConsoleTab().getTab());
 		tabPane.getTabs().add(new Tab("Progress"));
 
 	}
