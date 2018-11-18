52b2e7fe8c15c14c840b027e338cccad2f2eeee1
unixxcorn <siwawat.srisuk@gmail.com>
2018-11-19 03:58:52 +0700

    Update console

--- Signy IDE/src/main/java/signy/ide/core/module/SConsoleTab.java
+++ Signy IDE/src/main/java/signy/ide/core/module/SConsoleTab.java
@@ -1,7 +1,8 @@
 package signy.ide.core.module;
 
 import java.io.*;
-import java.util.ArrayList;
+
+import javax.swing.JTextArea;
 
 import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
@@ -17,6 +18,8 @@ public class SConsoleTab {
 	private Process p;
 	private ProcessBuilder builder;
 	private Thread thread;
+	private ProcessBuilderCommand pbc;
+	
 	public SConsoleTab() {
 		this("Console");
 	}
@@ -30,61 +33,87 @@ public class SConsoleTab {
 		p0.setCenter(textArea);
 		p0.setBottom(commandtf);
 		this.tab.setContent(p0);
+		this.tab.setText(title);
 		commandtf.setOnKeyPressed(new EventHandler<KeyEvent>() {
 			public void handle(KeyEvent ke) {
 				if (ke.getCode() == KeyCode.ENTER) {
+					
+					if(pbc != null && pbc.isRunning()) {
+						pbc.stop();
+					}
 					runCommand(commandtf.getText());
+					tab.setText(commandtf.getText());
+					commandtf.setText("");
 				}
-
 			}
 		});
-		this.tab.setText(title);
+		
 
 	}
 
 	public void runCommand(String Command) {
-		thread = new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				try {
-					String line = null;
-					builder = new ProcessBuilder("cmd.exe", "/c", Command);
-					builder.redirectErrorStream(true);
-					p = builder.start();
-					content += "\n > " + Command +" \n ";
-
-					BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
-					while ((line = in.readLine()) != null) {
-						content += line + "\n";
-						textArea.setText(content);
-					}
-
-					BufferedReader er = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-					while ((line = er.readLine()) != null) {
-						content += line + "\n";
-						textArea.setText(content);
-					}
-
-					p.waitFor();
-					p.getInputStream().close();
-					p.getErrorStream().close();
-				} catch (IOException | InterruptedException e) {
-					content += e.getMessage();
-					textArea.setText(content);
-				}
-				p.destroy();
-				commandtf.setText("");
-				
-			}
-			
-		});
-		thread.start();
+		try {
+			pbc = new ProcessBuilderCommand(textArea, Command);
+		} catch (InterruptedException | IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
 	public Tab getTab() {
 		return this.tab;
+	}
+	
+	public class ProcessBuilderCommand {
+		private Process p;
+		private ProcessBuilder pb;
+		private Thread thread;
+		private boolean running = false;
+		
+		public ProcessBuilderCommand(TextArea ta, String Command) throws InterruptedException, IOException {
+			pb = new ProcessBuilder(Command);
+			
+			ta.appendText(System.getProperty("user.dir") + " > " + Command + "\n");
+			this.running = true;
+			thread = new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+					try {
+						String line = null;
+						p = pb.start();
+
+						BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+						while ((line = in.readLine()) != null) {
+							ta.appendText("    " + line + "\n");
+						}
+
+						BufferedReader er = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+						while ((line = er.readLine()) != null) {
+							ta.appendText(" [ERROR] " + line + "\n");
+						}
+
+						p.getInputStream().close();
+						p.getErrorStream().close();
+					} catch (IOException e) {
+						System.out.println(e.getMessage());
+					}
+					p.destroy();
+					
+				}
+				
+			});
+			thread.start();
+			
+		}
+		public void stop() {
+			p.destroy();
+			this.running = false;
+		}
+		
+		public boolean isRunning() {
+			return this.running;
+		}
 	}
 
 }
