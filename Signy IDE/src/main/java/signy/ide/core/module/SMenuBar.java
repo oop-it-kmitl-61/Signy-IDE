a5b1cfe157b6c6753c7b55e567f3fbf761117f2f
vieww <vieww@DESKTOP-97RLPEA>
2018-11-24 22:28:13 +0700

    update "Menu bar"

--- Signy IDE/src/main/java/signy/ide/core/module/SMenuBar.java
+++ Signy IDE/src/main/java/signy/ide/core/module/SMenuBar.java
@@ -16,7 +16,9 @@ public class SMenuBar {
 	private Main mainApp;
 	private SEditorPane editor;
 
-	private Menu fileMenu, editMenu, selectionMenu, viewMenu, goMenu, helpMenu;
+	private Menu fileMenu, editMenu, selectionMenu, viewMenu, helpMenu;
+	
+	private String TempItem = "";
 
 	public SMenuBar(FXMLDocumentController controller) {
 
@@ -103,29 +105,162 @@ public class SMenuBar {
 		// 2. Edit Menu
 		Menu editMenu = new Menu("_Edit");
 
+		MenuItem UndoItem = new MenuItem("Undo");
+		UndoItem.setAccelerator(
+				new KeyCodeCombination(KeyCode.Z, KeyCombination.SHORTCUT_DOWN));
+		UndoItem.setOnAction(e -> {
+			editor.getCurrentActiveTab().getTextArea().undo();
+		});
+		
+		MenuItem RedoItem = new MenuItem("Redo");
+		RedoItem.setAccelerator(
+				new KeyCodeCombination(KeyCode.Y, KeyCombination.SHORTCUT_DOWN));
+		RedoItem.setOnAction(e -> {
+			editor.getCurrentActiveTab().getTextArea().redo();
+		});
+		
+		MenuItem CutItem = new MenuItem("Cut");
+		CutItem.setAccelerator(
+				new KeyCodeCombination(KeyCode.X, KeyCombination.SHORTCUT_DOWN));
+		CutItem.setOnAction(e -> {
+			if (editor.getCurrentActiveTab().getTextArea().getSelectedText() != null) {
+				TempItem = editor.getCurrentActiveTab().getTextArea().getSelectedText();
+				editor.getCurrentActiveTab().getTextArea().replaceSelection("");;
+			}
+			//System.out.println(editor.getCurrentActiveTab().getTextArea().getText());
+		});
+		
+		MenuItem CopyItem = new MenuItem("Copy");
+		CopyItem.setAccelerator(
+				new KeyCodeCombination(KeyCode.C, KeyCombination.SHORTCUT_DOWN));
+		CopyItem.setOnAction(e -> {
+			if (editor.getCurrentActiveTab().getTextArea().getSelectedText() != null) {
+				TempItem = editor.getCurrentActiveTab().getTextArea().getSelectedText();
+			}
+		});
+
+		MenuItem PasteItem = new MenuItem("Paste");
+		PasteItem.setAccelerator(
+				new KeyCodeCombination(KeyCode.V, KeyCombination.SHORTCUT_DOWN));
+		PasteItem.setOnAction(e -> {
+			if (TempItem != "") {
+				editor.getCurrentActiveTab().getTextArea().replaceSelection(TempItem);
+			}
+		});
+		
+		MenuItem FindItem = new MenuItem("Find");
+		FindItem.setOnAction(e -> {
+			System.out.println(e.getSource() + " didn't have any action yet");
+		});
+		
+		MenuItem ReplaceItem = new MenuItem("Replace");
+		ReplaceItem.setOnAction(e -> {
+			System.out.println(e.getSource() + " didn't have any action yet");
+		});
+		
+		MenuItem SearchInWorkspace = new MenuItem("Search In Workspace");
+		SearchInWorkspace.setOnAction(e -> {
+			System.out.print(e.getSource() + " didn't have any action yet");
+		});
+		
+		editMenu.getItems().addAll(UndoItem, RedoItem, new SeparatorMenuItem(), 
+				CutItem, CopyItem, PasteItem ,new SeparatorMenuItem(), FindItem, ReplaceItem, SearchInWorkspace);
+		
 		// 3. Selection Menu
 		Menu selectionMenu = new Menu("_Selection");
 
+		MenuItem SelectAllItem = new MenuItem("Select All");
+		SelectAllItem.setAccelerator(
+				new KeyCodeCombination(KeyCode.A, KeyCombination.SHORTCUT_DOWN));
+		SelectAllItem.setOnAction(e -> {
+			editor.getCurrentActiveTab().getTextArea().selectAll();
+		});
+		
+		MenuItem ExpandSelectItem = new MenuItem("Expand Select");
+		ExpandSelectItem.setOnAction(e -> {
+			editor.getCurrentActiveTab().getTextArea().selectParagraph();
+		});
+
+		MenuItem ShrinkSelectItem = new MenuItem("Shrink Select");
+		ShrinkSelectItem.setOnAction(e -> {
+			
+		});
+		
+		MenuItem CopyLineUp = new MenuItem("Copy Line Up");
+		CopyLineUp.setOnAction(e -> {
+			System.out.print(e.getSource() + " didn't have any action yet");
+		});
+		
+		MenuItem CopyLineDown = new MenuItem("Copy Line Down");
+		CopyLineDown.setOnAction(e -> {
+			System.out.print(e.getSource() + " didn't have any action yet");
+		});
+		
+		MenuItem MoveLineUp = new MenuItem("Move Line Up");
+		MoveLineUp.setOnAction(e -> {
+			System.out.println(e.getSource() + " didn't have any action yet");
+		});
+		
+		MenuItem MoveLineDown = new MenuItem("Move Line Down");
+		MoveLineDown.setOnAction(e -> {
+			System.out.print(e.getSource() + " didn't have any action yet");
+		});
+		
+		selectionMenu.getItems().addAll(SelectAllItem, ExpandSelectItem, 
+				ShrinkSelectItem ,new SeparatorMenuItem(),CopyLineUp, CopyLineDown, MoveLineUp, MoveLineDown,
+				new SeparatorMenuItem());
+		
 		// 4. View Menu
 		Menu viewMenu = new Menu("_View");
 
-		// 5. Go Menu
-		Menu goMenu = new Menu("_Go");
+		MenuItem Files = new MenuItem("Files");
+		Files.setOnAction(e -> {
+			System.out.print(e.getSource() + " didn't have any action yet");
+		});
+		
+		MenuItem Search = new MenuItem("Search");
+		Search.setOnAction(e -> {
+			System.out.print(e.getSource() + " didn't have any action yet");
+		});
+		
+		MenuItem ToggleBottomPanel = new MenuItem("Toggle Bottom Panels");
+		ToggleBottomPanel.setOnAction(e -> {
+			System.out.print(e.getSource() + " didn't have any action yet");
+		});
+		
+		MenuItem CollaspeAllsidePanel = new MenuItem("Collaspe All Side Panels");
+		CollaspeAllsidePanel.setOnAction(e -> {
+			System.out.print(e.getSource() + " didn't have any action yet");
+		});
 
+		viewMenu.getItems().addAll(Files,Search ,new SeparatorMenuItem(),
+				ToggleBottomPanel,CollaspeAllsidePanel);
+		
 		// 6. Help Menu
 		Menu helpMenu = new Menu("_Help");
+		
+		MenuItem About = new MenuItem("About");
+		About.setOnAction(e -> {
+			System.out.print(e.getSource() + " didn't have any action yet");
+		});
+		
+		MenuItem Document = new MenuItem("Documentation");
+		Document.setOnAction(e -> {
+			System.out.print(e.getSource() + " didn't have any action yet");
+		});
+		
+		helpMenu.getItems().addAll(About,Document);
 
 		this.fileMenu = fileMenu;
 		this.editMenu = editMenu;
 		this.selectionMenu = selectionMenu;
 		this.viewMenu = viewMenu;
-		this.goMenu = goMenu;
 		this.helpMenu = helpMenu;
 
 	}
 
 	public MenuBar getMenuBar() {
-		return new MenuBar(fileMenu, editMenu, selectionMenu, viewMenu, goMenu, helpMenu);
+		return new MenuBar(fileMenu, editMenu, selectionMenu, viewMenu, helpMenu);
 	}
 
 }
