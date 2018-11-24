e2ccb70797a1dce86eb52209d178bfdcf151e484
Thitichote <31314155+Thitichote@users.noreply.github.com>
2018-11-25 04:44:03 +0700

    add find and replace , fixed loop bug (injection)

---
+++ Signy IDE/src/main/java/signy/ide/controls/items/SearchItem.java
@@ -0,0 +1,34 @@
+package signy.ide.controls.items;
+
+public class SearchItem {
+	
+	private String toString;
+	private int line;
+	private int l1; // index
+	private int l2; // end index length of string
+	
+	public SearchItem(int line ,String show, int l1,int l2) {
+
+		this.line = line+1;
+		toString = "Line : " + this.line + " " + show;
+		this.l1 = l1;
+		this.l2 = l2;
+	}
+	
+	public int getLine() {
+		return line;
+	}
+	
+	@Override
+	public String toString() {
+		return toString.trim();
+	}
+	
+	public int getIndex() {
+		return l1;
+	}
+	
+	public int getLength() {
+		return l2;
+	}
+}
